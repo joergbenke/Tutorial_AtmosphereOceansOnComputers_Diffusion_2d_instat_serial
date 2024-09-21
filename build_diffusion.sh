ee687fc592c850e07bd04cdcda7cc5417fc5cec4
Joerg Benke <j.benke@fz-juelich.de>
2024-09-21 22:22:04 +0200

    Preparation for RSE workshop

--- build_diffusion.sh
+++ build_diffusion.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rm -r build
+
+. ./env_JUWELS_Stage2024.sh
+rm -rf build
 
 cmake -S src -B build
 cmake --build ./build -- VERBOSE=1
---
+++ env_JUWELS_Stage2024.sh
@@ -0,0 +1,9 @@
+
+module --force purge
+
+module load Stages/2024
+module load GCC
+module load ParaStationMPI
+module load CMake
+
+module list
